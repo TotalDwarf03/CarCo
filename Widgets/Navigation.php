5a7f6d569f2f340ef0e37b99fdf31658cb7f97d2
Kieran Pritchard <kieran.pritchard15@ccyd.org.uk>
2023-11-01 14:19:12 +0000

    finished navigation widget

--- Widgets/Navigation.php
+++ Widgets/Navigation.php
@@ -1,9 +1,11 @@
 <?php
     function getActivePage(string $PageName){
+        // Checks if the current page name is equal to $PageName
         return (basename($_SERVER['PHP_SELF']) == $PageName) ? "class='active'" : '';
     }
 
     function showOnPermissions(int $permissionID, string $PageName){
+        // Controls which pages show based on user permissions
         if(isset($_SESSION['UserID'])) {
             // If Logged in
 
@@ -17,11 +19,25 @@
             else{
                 if($_SESSION['UserType'] == 'Staff'){
                     // If Staff
-                    // Put permission logic here :)
+                    if(in_array($permissionID, $_SESSION['UserPermissions'])){
+                        // If passed $permissionID in the permission list, show
+                        return '';
+                    }
+                    else{
+                        // If not, hide
+                        return "style='display:none;'";
+                    }
                 }
                 else {
                     // If Customer
-                    return "style='display:none;'";
+                    if($PageName == 'vieworders'){
+                        // If Order Page, show
+                        return '';
+                    }
+                    else {
+                        // If not, hide
+                        return "style='display:none;'";
+                    }
                 }
             }
         }
@@ -36,6 +52,7 @@
                 return "style='display:none;'";
             }
             else {
+                // Hide other pages
                 return "style='display:none;'";
             }
         }
@@ -48,10 +65,10 @@
     <nav>
         <ul>
             <li><a <?php echo(getActivePage('index.php')); ?> href='index.php'>Home</a></li>
-            <li><a <?php echo(getActivePage('vieworders.php') . ' ' . showOnPermissions(-1, 'vieworders')); ?> href='vieworders.php'>Orders</a></li>
+            <li><a <?php echo(getActivePage('vieworders.php') . ' ' . showOnPermissions(5, 'vieworders')); ?> href='vieworders.php'>Orders</a></li>
             <li><a <?php echo(getActivePage('viewproducts.php')); ?> href='viewproducts.php'>Products</a></li>
-            <li><a <?php echo(getActivePage('viewcustomers.php') . ' ' . showOnPermissions(-1, 'viewcustomers')); ?> href='viewcustomers.php'>Customers</a></li>
-            <li><a <?php echo(getActivePage('viewstaff.php') . ' ' . showOnPermissions(-1, 'viewstaff')); ?> href='viewstaff.php'>Staff</a></li>
+            <li><a <?php echo(getActivePage('viewcustomers.php') . ' ' . showOnPermissions(4, 'viewcustomers')); ?> href='viewcustomers.php'>Customers</a></li>
+            <li><a <?php echo(getActivePage('viewstaff.php') . ' ' . showOnPermissions(2, 'viewstaff')); ?> href='viewstaff.php'>Staff</a></li>
             <li><a <?php echo(getActivePage('login.php') . ' ' . showOnPermissions(-1, 'login')); ?> href='login.php'>Login</a></li>
             <li><a <?php echo(getActivePage('logout.php') . ' ' . showOnPermissions(-1, 'logout')); ?> href='logout.php'>Logout</a></li>
         </ul>
