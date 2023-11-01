62d942b04167f718d92ffb74981f1948534e84b5
Kieran Pritchard <kieran.pritchard15@ccyd.org.uk>
2023-11-01 15:17:05 +0000

    added extra pages ready for other stuff :)

---
+++ viewcustomers.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="all" href="css/style.css">
+    <title>CarCo - Customers</title>
+</head>
+<body class="CentrePage">
+    <?php include("Widgets/navigation.php") ?>
+
+    <!-- Stuff Here -->
+</body>
+</html>
