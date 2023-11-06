c856645b88ca32b98b84482466b5f382d74c07f9
Kieran Pritchard <kieran.pritchard15@ccyd.org.uk>
2023-11-06 13:42:29 +0000

    Added Footer and Homepage

--- index.php
+++ index.php
@@ -10,5 +10,30 @@
 </head>
 <body class="CentrePage">
     <?php include("Widgets/navigation.php") ?>
+
+    <div class="content">
+        <h2 style="padding-top: 10px;">Welcome to CarCo's Online Portal!</h2>
+
+        <img src="Images/CarParts.jpg" alt="Image of Car Parts" height="300px">
+
+        <p>
+            To get started, please <a href="login.php"><b>login</b></a> to your account. 
+            <br>
+            If you need to create an account please get in contact with one of our members of staff. 
+            <br>
+            <br>
+            If you're new, why not take a look at our <a href="viewproducts.php"><b>Range of Products</b></a>?
+        </p>
+
+        <h2>Need Some Help?</h2>
+        <p>
+            Please contact us on <a href="tel:01656 123123">01656 123123</a> or email us at <a href="mailto:Help@CarCo.co.uk">Help@CarCo.co.uk</a>.
+            <br>
+            Our operating hours are 09:00-17:00 Monday-Saturday.
+        </p>
+        <br>
+    </div>
+
+    <?php include("Widgets/footer.php") ?>
 </body>
 </html>
